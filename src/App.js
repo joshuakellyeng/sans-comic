1832a9d01874c67637a206024425b4caf8b8bf33
Joshua Kelly <joshuakellyeng@gmail.com>
2022-01-30 19:19:29 -0500

    added pages and linked using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 //components
-import Footer from './components/Footer';
-import Hero from './components/Hero';
 import NavBar from './components/NavBar'
+import Footer from './components/Footer';
 //pages
 import Home from './pages/Home';
 import Promos from './pages/Promos';
 import Series from './pages/Series';
 import Checkout from './pages/Checkout';
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="promos" element={<Promos/>} />
+        <Route path="series" element={<Series/>} />
+        <Route path="contact" element={<Contact/>} />
+        <Route path="checkout" element={<Checkout/>} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 	return (
 		<div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded">
 			<div className="flex-1 flex items-center sm:items-stretch sm:justify-start">
 				<div className="flex-shrink-0 flex items-center">
+					<Link to="/">
 					<img
 						className="block lg:hidden h-8 w-auto"
 						src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
 						alt="Workflow"
-					/>
+						/>
 					<img
 						className="hidden lg:block h-8 w-auto"
 						src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
 						alt="Workflow"
-					/>
+						/>
+					</Link>
 				</div>
 			</div>
 			<div className="px-2 mx-2">
 				<div className="items-stretch hidden lg:flex">
-					<a className="btn btn-ghost btn-sm rounded-btn">Latest Releases</a>
-					<a className="btn btn-ghost btn-sm rounded-btn">Series</a>
-					<a className="btn btn-ghost btn-sm rounded-btn">Contact</a>
+					<Link to='promos' className="btn btn-ghost btn-sm rounded-btn">Latest Releases</Link>
+					<Link to='series' className="btn btn-ghost btn-sm rounded-btn">Series</Link>
+					<Link to='contact' className="btn btn-ghost btn-sm rounded-btn">Contact</Link>
 				</div>
 			</div>
 			<div className="flex-none">
-				<button className="btn btn-square btn-ghost">
+				<Link to='checkout' className="btn btn-square btn-ghost">
 					<svg
 						className="w-6 h-6"
 						fill="none"
@@ -40,7 +43,7 @@ const NavBar = () => {
 							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
 						></path>
 					</svg>
-				</button>
+				</Link>
 			</div>
 		</div>
 	);
